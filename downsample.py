da76db600fdd1b345a70b19f25d7cc3cbe884de1
Behnam Ghassemi <behnamx@icloud.com>
2018-04-10 16:13:36 -0700

    Finished downsampling

--- downsample.py
+++ downsample.py
@@ -5,17 +5,17 @@ import cv2
 high_res_directory = 'input/'
 low_res_directory = 'output/'
 
-# Downsample HR images
+# Downsample High res images in output directory
 for filename in os.listdir(high_res_directory):
 
-    # Read HR image
+    # Read high res images from input directory
     high_res = cv2.imread(os.path.join(high_res_directory, filename))
 
-    # Blur with Gaussian kernel of width sigma=1
-    high_res = cv2.GaussianBlur(hr_img, (0, 0), 1, 1)
+    # Blur images with gaussian
+    high_res = cv2.GaussianBlur(high_res, (0, 0), 1, 1)
 
 
-    # Downsample image 4x
+    # Resize 1/4
     low_res_4x = cv2.resize(high_res, (0, 0), fx=0.25, fy=0.25,
                            interpolation=cv2.INTER_CUBIC)
 
